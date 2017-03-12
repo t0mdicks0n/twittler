401c62a28cff015ca70787ad3f3faa9ddb86769f
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2017-03-12 11:21:29 +0100

    "Added a element just above the stream that automatically shows the user if they have any new tweets to show. Next step is to make that element clickable so that it updates the stream with the new tweets."

--- application.js
+++ application.js
@@ -15,24 +15,6 @@ $(document).ready(function(){
 */
 
 $(document).ready(function() {
-  /*
-  var $body = $('body');
-  $body.find('.content').html('');
-
-  var index = streams.home.length - 1;
-  while(index >= 0) {
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="content"><div class="timestamp"></div><div class="user"></div><div class="tweet"></div></div>');
-    
-    $tweet.find('.user').text('@' + tweet.user);
-    $tweet.find('.timestamp').text(tweet.created_at.toLocaleString());
-    $tweet.find('.tweet').text(tweet.message);
-    
-    $tweet.appendTo($body);
-    index -= 1;
-  }
-  */
-
   var INDEX_IN_TWEETS = 0;
   var $body = $('body').find('.stream');
   $body.find('.stream').html('');
@@ -50,17 +32,19 @@ $(document).ready(function() {
       $tweet.find('.timestamp').text(tweet.created_at.toLocaleString());
       $tweet.find('.tweet').text(tweet.message);
       
-      //$tweet.appendTo($body);
       $tweet.prependTo($body);
-
     }
   }
 
-  // Något är trasig med ovan. Hur fasiken ska jag kunna få ut de senaste Tweetsen genom att ha en knapp längst upp på sidan som säger
-  // "Hämta senaste Tweets" och skjuter ner de tidigare tweetsen längre ner på samma sida.
-  // Jag har ju nu appendTo, varför kan jag inte helt enkelt göra dess motsvarighet prependTo(), som push med arrays liksom. Men vad
-  // händer då med indexet?
+  // Runs constantly after a fixed interval and updates how many unread tweets the user has.
+  setInterval(streamUpload, 5000);
 
+  function streamUpload() {
+    var newTweets = streams.home.length - INDEX_IN_TWEETS;
+    if (newTweets > 0) {
+      $('body').find('#more-tweets').html("You have <strong>" + newTweets + "</strong> new tweets to show. Click to show them.");
+    }
+  }
 
 });
 
